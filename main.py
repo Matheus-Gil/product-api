4d60b7ba46ce123dab1b8cf8a4f9cf5182506531
Matheus-Gil <matheussoprangil@gmail.com>
2026-05-11 12:41:03 -0400

    Initial FastAPI CRUD API with SQLite

---
+++ main.py
@@ -0,0 +1,97 @@
+from fastapi import FastAPI
+from schemas import Product
+from database import engine, Base
+import models
+from database import SessionLocal
+
+app = FastAPI()
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def home():
+    return {"message": "hello world"}
+
+@app.get("/products")
+def get_products():
+    db = SessionLocal()
+
+    products = db.query(models.Product).all()
+
+    db.close()
+
+    return products 
+
+@app.get("/products/{product_id}")
+def get_by_id(product_id: int):
+    db = SessionLocal()
+
+    product = db.query(models.Product).filter(
+        models.Product.id == product_id
+    ).first()
+
+    db.close()
+
+    if product:
+        return product
+
+    return {"error": "Product not found"}
+
+@app.post("/products")
+def create_product(product: Product):
+    db = SessionLocal()
+
+    new_product = models.Product(
+        name=product.name,
+        price=product.price,
+        manufacturer=product.manufacturer,
+        stock=product.stock
+    )
+
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+
+    db.close()
+
+    return new_product
+
+@app.delete("/products/{product_id}")
+def delete_product(product_id: int):
+    db = SessionLocal()
+
+    product = db.query(models.Product).filter(
+        models.Product.id == product_id
+    ).first()
+
+    if not product:
+        db.close()
+        return {"error": "Product not found"}
+
+    db.delete(product)
+    db.commit()
+    db.close()
+
+    return {"message": "Product deleted",
+            "deleted item": product.name,
+            "id": product.id}
+
+@app.put("/products/{product_id}")
+def update_product(product_id: int, updated_product: Product):
+    db = SessionLocal()
+
+    product = db.query(models.Product).filter(
+        models.Product.id == product_id
+    ).first()
+
+    if not product:
+        db.close()
+        return {"error": "Product not found"}
+
+    product.name = updated_product.name
+    product.price = updated_product.price
+
+    db.commit()
+    db.refresh(product)
+    db.close()
+
+    return product
